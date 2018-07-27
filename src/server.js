ea47822cb64c097e8e137bd709e2ce9063bf8d78
martingaston <squidman@gmail.com>
2018-07-27 13:07:47 +0100

    updated config for heroku push

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const http = require("http");
 const router = require("./router");
 
 const server = http.createServer(router);
-const port = process.env.PORT;
+const port = process.env.PORT || 4000;
 server.listen(port, function() {
   console.log(`Server is listening on port ${port}`);
 });
